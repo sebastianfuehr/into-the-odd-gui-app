a8d614e6bef7d0688c2a76ba0a51d611cd7f2ff2
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-28 16:20:44 +0300

    test: implement tests for the note class
    Verifies repr_json() method for note entities, as well as their JSON de-
    and encoding.

--- tests/controller/test_json_controller.py
+++ tests/controller/test_json_controller.py
@@ -1,7 +1,9 @@
 import json
+from datetime import datetime
 
 from itom.controller.json_controller import ItomJSONDecoderFunction, ItomJSONEncoder
 from itom.model.character import Character
+from itom.model.utils import Note
 
 
 def test_json_encode_character(simple_character_ulf: Character) -> None:
@@ -30,3 +32,23 @@ def test_json_decode_character(simple_character_ulf: Character) -> None:
     json_file = json.dumps(simple_character_ulf.repr_json(), cls=ItomJSONEncoder)
     decoded_character = json.loads(json_file, object_hook=ItomJSONDecoderFunction)
     assert decoded_character == simple_character_ulf
+
+
+def test_json_encode_note() -> None:
+    date = datetime(2023, 7, 28, 15, 15, 56)
+    text = "This is a note."
+    new_note = Note(creation_date=date, text=text)
+    assert json.dumps(new_note.repr_json(), cls=ItomJSONEncoder) == (
+        '{"__type__": "Note", '
+        '"creation_date": "2023-07-28T15:15:56", '
+        '"text": "This is a note."}'
+    )
+
+
+def test_json_decode_note() -> None:
+    date = datetime(2023, 7, 28, 15, 15, 56)
+    text = "This is a note."
+    new_note = Note(creation_date=date, text=text)
+    json_file = json.dumps(new_note.repr_json(), cls=ItomJSONEncoder)
+    decoded_note = json.loads(json_file, object_hook=ItomJSONDecoderFunction)
+    assert decoded_note == new_note
