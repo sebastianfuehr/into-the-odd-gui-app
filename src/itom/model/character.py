826d82eb8f4057523b4351ad74dd7238ae280163
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-28 08:23:46 +0300

    feat: implement ExperienceLevel enum

---
+++ src/itom/model/character.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class ExperienceLevel(Enum):
+    NOVICE = "Novice"
+    PROFESSIONAL = "Professional"
+    EXPERT = "Expert"
+    VETERAN = "Veteran"
+    MASTER = "Master"
+    BEYOND = "Beyond"
