47572c304b8f51ae0c98821a5e2de140a77fab2b
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-30 13:34:37 +0300

    test: add full_character_torsten fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
-from itom.model.character import Character
+from itom.model.character import Character, ExperienceLevel
+from utils.ito_mock_data_utils import generate_random_notes
 
 
 @pytest.fixture
@@ -12,4 +13,27 @@ def simple_character_ulf() -> Character:
         willpower=(11, 11),
         hit_points=(6, 6),
         purse=(0, 50, 10),
+    )
+
+
+@pytest.fixture
+def full_character_torsten() -> Character:
+    notes = generate_random_notes(3)
+    return Character(
+        name="Torsten",
+        strength=(10, 10),
+        dexterity=(12, 12),
+        willpower=(13, 13),
+        hit_points=(8, 8),
+        purse=(25, 89, 121),
+        critical_damage=False,
+        armor=2,
+        advantages=["Good looking", "Quick thinker"],
+        disadvantages=["Curiousity"],
+        possessions=None,
+        weapons=None,
+        notes=notes,
+        experience_level=ExperienceLevel.PROFESSIONAL,
+        arcana=None,
+        enterprises=None,
     )
