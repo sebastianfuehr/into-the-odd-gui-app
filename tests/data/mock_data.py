dbf94a2c1bf9d27b8f2633062dda8b73e28823bd
Sebastian Führ <s.fuehr1@gmail.com>
2023-07-30 10:06:19 +0300

    test: add mock data module

---
+++ tests/data/mock_data.py
@@ -0,0 +1,26 @@
+# Comments have been generated via randomiandfy.com
+COMMENTS = [
+    "This is a random comment.",
+    "A comment for testing purposes.",
+    "Funny, you are funny, but thing is that being funny isn't enough anymore.",
+    "Can't read? Just zoom in, what's the problem? It is a very simple solution.",
+    "Top meme? I guess it is “Hey there miss”, so many people love this meme.",
+    (
+        "I can't even work in this environment, noise is too loud and the temperature"
+        " is way too low."
+    ),
+    "Guys do you think that world which is present in WoW is absolutely amazing?",
+    (
+        "Which movie was the funniest of all? I think Very Scary Movie was super"
+        " hilarious.How do you usually perform disk check? It takes too much time, is"
+        " there any quicker way?"
+    ),
+    (
+        "Bet it felt good, to swim in the sea after all these months of working, I"
+        " could use vacation as well."
+    ),
+    (
+        "Using other people's kindness for your own egoistic interests is a very bad"
+        " thing to do."
+    ),
+]
