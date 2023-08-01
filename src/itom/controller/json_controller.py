5f2aa46e1ef5d14a410c2475695e76de0715ffbe
Sebastian Führ <s.fuehr1@gmail.com>
2023-08-01 09:16:03 +0300

    feat: implement misc. item classes json serialization

--- src/itom/controller/json_controller.py
+++ src/itom/controller/json_controller.py
@@ -5,7 +5,7 @@ from typing import Any
 import dateutil.parser
 
 from itom.model.character import Character
-from itom.model.misc_models import Enterprise, Note
+from itom.model.misc_models import Arcanum, Armor, Enterprise, Item, Note, Weapon
 
 
 class ItomJSONDecoder(json.JSONDecoder):
@@ -18,7 +18,9 @@ class ItomJSONDecoder(json.JSONDecoder):
     def __init__(self) -> None:
         json.JSONDecoder.__init__(self, object_hook=self.object_hook)
 
-    def object_hook(self, json_dict: dict) -> Character | Note | Enterprise | dict:
+    def object_hook(
+        self, json_dict: dict
+    ) -> Character | Note | Item | Enterprise | dict:
         if "__type__" in json_dict and json_dict["__type__"] == Character.__name__:
             strength = int(json_dict["strength"][0]), int(json_dict["strength"][1])
             dexterity = int(json_dict["dexterity"][0]), int(json_dict["dexterity"][1])
@@ -39,15 +41,83 @@ class ItomJSONDecoder(json.JSONDecoder):
                 hit_points=hit_points,
                 purse=purse,
                 critical_damage=json_dict["critical_damage"],
-                armor=json_dict["armor"],
                 advantages=json_dict["advantages"],
                 disadvantages=json_dict["disadvantages"],
                 possessions=json_dict["possessions"],
+                armor_items=json_dict["armor_items"],
                 weapons=json_dict["weapons"],
                 notes=json_dict["notes"],
                 experience_level=json_dict["experience_level"],
                 arcana=json_dict["arcana"],
                 enterprises=json_dict["enterprises"],
+            )
+        if "__type__" in json_dict and json_dict["__type__"] == Item.__name__:
+            worth = None
+            if json_dict["worth"]:
+                worth = (
+                    int(json_dict["worth"][0]),
+                    int(json_dict["worth"][1]),
+                    int(json_dict["worth"][2]),
+                )
+            return Item(
+                name=json_dict["name"],
+                description=json_dict["description"],
+                bulky=json_dict["bulky"],
+                worth=worth,
+                image_file_path=json_dict["image_file_path"],
+            )
+        if "__type__" in json_dict and json_dict["__type__"] == Weapon.__name__:
+            worth = None
+            if json_dict["worth"]:
+                worth = (
+                    int(json_dict["worth"][0]),
+                    int(json_dict["worth"][1]),
+                    int(json_dict["worth"][2]),
+                )
+            return Weapon(
+                name=json_dict["name"],
+                description=json_dict["description"],
+                bulky=json_dict["bulky"],
+                worth=worth,
+                image_file_path=json_dict["image_file_path"],
+                dmg_die=json_dict["dmg_die"],
+                amt_dice=json_dict["amt_dice"],
+                weapon_type=json_dict["weapon_type"],
+            )
+        if "__type__" in json_dict and json_dict["__type__"] == Armor.__name__:
+            worth = None
+            if json_dict["worth"]:
+                worth = (
+                    int(json_dict["worth"][0]),
+                    int(json_dict["worth"][1]),
+                    int(json_dict["worth"][2]),
+                )
+            return Armor(
+                name=json_dict["name"],
+                description=json_dict["description"],
+                bulky=json_dict["bulky"],
+                worth=worth,
+                image_file_path=json_dict["image_file_path"],
+                protection_value=json_dict["protection_value"],
+                armor_type=json_dict["armor_type"],
+                equipped=json_dict["equipped"],
+            )
+        if "__type__" in json_dict and json_dict["__type__"] == Arcanum.__name__:
+            worth = None
+            if json_dict["worth"]:
+                worth = (
+                    int(json_dict["worth"][0]),
+                    int(json_dict["worth"][1]),
+                    int(json_dict["worth"][2]),
+                )
+            return Arcanum(
+                name=json_dict["name"],
+                description=json_dict["description"],
+                bulky=json_dict["bulky"],
+                worth=worth,
+                image_file_path=json_dict["image_file_path"],
+                idx_nbr=json_dict["idx_nbr"],
+                arcanum_type=json_dict["arcanum_type"],
             )
         if "__type__" in json_dict and json_dict["__type__"] == Note.__name__:
             date = dateutil.parser.parse(json_dict["creation_date"])
