38d7d9a20403695ba8968887ef778fbdde4d21e7
Sebastian Führ <s.fuehr1@gmail.com>
2023-08-06 15:39:53 +0300

    style: add GUIStructure class for separated structural GUI information

--- src/itom/app.py
+++ src/itom/app.py
@@ -1,8 +1,11 @@
-from typing import Optional
+from typing import TYPE_CHECKING, Optional
 
 from components.main_frame import MainFrame
 from controller.world_controller import WorldController
-from model.definitions import Settings
+from model.definitions import GUIStructure, Settings
+
+if TYPE_CHECKING:
+    from ttkbootstrap.style import Style
 
 
 class App:
@@ -10,20 +13,34 @@ class App:
     This class doesn't inherit from ttkbootstrap.Window to allow the
     implementation of pop up windows which trigger 'before' the main
     window opens.
+
+    Attributes
+        guis(GUIStructure):
+        main_frame(MainFrame): The entry point for the application's
+            GUI.
+        settings(Settings):
+        style(ttkbootstrap.style.Style)
+        world_controller(WorldController):
     """
 
     def __init__(self, settings_file_path: Optional[str] = None) -> None:
+        """
+        Args
+            settings_file_path(str): A path string towards an ini file
+                with user settings.
+        """
         self.settings = Settings(
             app_name="Itom",
             app_author="Sebastian Führ",
             settings_file_path=settings_file_path,
         )
-        self.gui = None
+        self.guis = GUIStructure(self.settings)
         self.world_controller = WorldController(self.settings)
         self.world = self.world_controller.load_world("Default")
 
+        self.style: Style = None
         theme = self.settings.config["appearance"]["theme"]
-        self.main_frame = MainFrame(themename=theme)
+        self.main_frame = MainFrame(app=self, themename=theme)
 
     def start_gui(self) -> None:
         """Start the application loop and open the GUI."""
--- src/itom/components/main_frame.py
+++ src/itom/components/main_frame.py
@@ -7,16 +7,39 @@ import ttkbootstrap as tb
 from itom.components.views import ListFrame
 
 if TYPE_CHECKING:
-    pass
+    from itom.app import App
 
 
 class MainFrame(tb.Window):  # type: ignore
-    def __init__(self, themename: str) -> None:
+    """The entrypoint for the application's GUI.
+
+    Attributes
+        content_frame(tb.Frame): The central frame of the application
+            which will be manipulated by the main menu to show
+            different content components.
+        main_menu(MainMenu): The main menu GUI component.
+        mm_key_var(tb.StringVar): Stores the currently selected main
+            menu object.
+    """
+
+    def __init__(self, app: "App", themename: str) -> None:
+        """
+        Forwards the created ttkbootstrap Style object to the
+        initiating App object.
+
+        Args
+            app(App): An app object to pass on to all child components as a
+                controller of app settings and visual structure
+                attributes.
+            themename(str): A ttkbootstrap theme name.
+        """
         super().__init__(
             title="Into the Odd Manager",
             themename=themename,
         )
+        self.app = app
 
+        self.app.style = self.style
         self.mm_key_var = tb.StringVar()
 
         self.main_menu: Optional[MainMenu] = None
@@ -26,7 +49,7 @@ class MainFrame(tb.Window):  # type: ignore
 
     def _build_gui_components(self) -> None:
         """Create the GUI elements inside this component."""
-        self.main_menu = MainMenu(self, self.mm_key_var)
+        self.main_menu = MainMenu(self, self.app, self.mm_key_var)
         self.main_menu.pack(fill="x")
 
         self.content_frame = ListFrame(self)
@@ -34,14 +57,25 @@ class MainFrame(tb.Window):  # type: ignore
 
 
 class MainMenu(tb.Frame):  # type: ignore
-    def __init__(self, master: tb.Frame, nav_key_var: tb.StringVar) -> None:
+    def __init__(self, master: tb.Frame, app: "App", nav_key_var: tb.StringVar) -> None:
         super().__init__(master=master)
+        self.app = app
         self.nav_key_var = nav_key_var
 
         # Button for world selection
-        lbl_world = tb.Label(self, text="World Name")
-        lbl_world.pack(side="left")
+        style_lbl_world = self.app.guis.buttons["world_selection"]
+        lbl_world = tb.Label(
+            self,
+            text="World Name",
+            font=style_lbl_world["font"],
+            background=self.app.style.colors.primary,
+        )
+        lbl_world.pack(
+            side="left", padx=style_lbl_world["padx"], pady=style_lbl_world["pady"]
+        )
 
         # Menu items
-        lbl_character = tb.Label(self, text="Characters")
+        lbl_character = tb.Label(
+            self, text="Characters", font=self.app.guis.fonts["Default"]
+        )
         lbl_character.pack(side="left")
